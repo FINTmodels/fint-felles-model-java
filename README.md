2fc520f54d9c16523751b6439e46bf3c214b317a
Frode Sjovatsen <frode@sjovatsen.net>
2017-03-25 22:18:58 +0100

    added relations + minor bug fixes

--- README.md
+++ README.md
@@ -1,3 +1,20 @@
-# fint-felles-model-java
+# FINT felles informasjonsmodeller for Java
 
-[ ![Download](https://api.bintray.com/packages/fint/maven/fint-felles-model-java/images/download.svg) ](https://bintray.com/fint/maven/fint-felles-model-java/_latestVersion)
+Inneholder felles informasjonsmodeller
+
+## Modeller
+### Felles
+* Adresse
+* Aktør
+* Begrep
+* Enhet
+* Identifikator
+* Kontaktinformasjon
+* Periode
+* Personavn
+### ISO
+* Kjønn
+* Landkode
+* Språk
+
+Se informasjonsmodell dokumentasjonen for mer informasjon: [https://dokumentasjon.felleskomponent.no](https://dokumentasjon.felleskomponent.no)
--- src/main/java/no/fint/model/felles/Person.java
+++ src/main/java/no/fint/model/felles/Person.java
@@ -11,17 +11,19 @@ import java.util.Date;
 @NoArgsConstructor
 @EqualsAndHashCode(callSuper=true)
 public class Person extends Aktor {
+    public static final String RELASJON_PERSON_PERSONALRESSURS = "urn:fint.no:person:personalressurs:person.fodselsnummer:personalressurs.ansattnummer";
+    public static final String RELASJON_PERSON_KJONN = "urn:fint.no:person:kjonn:person.fodselsnummer:kjonn.systemid";
+    public static final String RELASJON_PERSON_STATSBORGERSKAP = "urn:fint.no:person:statsborgerskap:person.fodselsnummer:statsborgerskap.systemid";
+
     private Identifikator fodselsnummer;
     private Personnavn navn;
-    private String kjonn;
     private Date fodselsdato;
     private Adresse bostedsadresse;
 
-    public Person(Kontaktinformasjon kontaktinformasjon, Adresse postadresse, Identifikator fodselsnummer, Personnavn navn, String kjonn, Date fodselsdato, Adresse bostedsadresse) {
+    public Person(Kontaktinformasjon kontaktinformasjon, Adresse postadresse, Identifikator fodselsnummer, Personnavn navn, Date fodselsdato, Adresse bostedsadresse) {
         super(kontaktinformasjon, postadresse);
         this.fodselsnummer = fodselsnummer;
         this.navn = navn;
-        this.kjonn = kjonn;
         this.fodselsdato = fodselsdato;
         this.bostedsadresse = bostedsadresse;
     }
