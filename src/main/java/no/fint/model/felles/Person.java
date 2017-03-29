a4c115b9bff603133acb7c8a1e1f29109416fe56
Jarle Hansen <jarle@jarlehansen.net>
2017-03-29 18:47:02 +0200

    WIP: started work on new relation format

--- src/main/java/no/fint/model/felles/Person.java
+++ src/main/java/no/fint/model/felles/Person.java
@@ -7,27 +7,31 @@ import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import no.fint.model.felles.iso.Kjonn;
 import no.fint.model.relation.Identifiable;
+import no.fint.model.relation.Relation;
 import no.fint.model.relation.RelationType;
 
 import java.util.Date;
+import java.util.List;
 
 @Data
 @NoArgsConstructor
 @EqualsAndHashCode(callSuper = true)
-public class Person extends Aktor implements Identifiable {
-    public static final String REL_ID_PERSONALRESSURS = new RelationType.Builder()
-            .namespace("fint.no").relationName("personalressurs").main(Person.class, "fodselsnummer").relatedId("personalressurs.ansattnummer").buildTypeString();
+public class Person extends Aktor implements FintModel { // Relations (getRelations())
 
-    public static final String REL_ID_KJONN = new RelationType.Builder()
-            .namespace("fint.no").relationName("kjonn").main(Person.class, "fodselsnummer").related(Kjonn.class, "systemid").buildTypeString();
+    public static enum Relasjonsnavn {
+        PERSONALRESSURS,
+        KJONN,
+        STATSBORGERSKAP
+    }
 
-    public static final String REL_ID_STATSBORGERSKAP = new RelationType.Builder()
-            .namespace("fint.no").relationName("statsborgerskap").main(Person.class, "fodselsnummer").relatedId("statsborgerskap.systemid").buildTypeString();
+    public static final String BASE = "https://api.felleskomponent.no/administrasjon/personal";
 
     private Identifikator fodselsnummer;
     private Personnavn navn;
     private Date fodselsdato;
     private Adresse bostedsadresse;
+    private List<Relation> relasjoner; // Relation.for(Person.Relasjonsnavn.PERSONALRESSURS).fromBase(BASE_URL).path("/personalressurs")
+                                        // Relation.for(Person.Relasjonsnavn.PERSONALRESSURS).link("http://localhost/personalressurs")
 
     public Person(Kontaktinformasjon kontaktinformasjon, Adresse postadresse, Identifikator fodselsnummer, Personnavn navn, Date fodselsdato, Adresse bostedsadresse) {
         super(kontaktinformasjon, postadresse);
@@ -37,9 +41,7 @@ public class Person extends Aktor implements Identifiable {
         this.bostedsadresse = bostedsadresse;
     }
 
-    @JsonIgnore
-    @Override
-    public String getId() {
-        return this.getFodselsnummer().getIdentifikatorverdi();
+    public List<Relation> getRelations() {
+        return relasjoner;
     }
 }
