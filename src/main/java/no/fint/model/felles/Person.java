2f73b16a2e025b1d3a8bd39fdba01cdc2dfa6b0d
jarlehansen <jarle@jarlehansen.net>
2017-03-30 21:17:04 +0200

    updated to new relation model

--- src/main/java/no/fint/model/felles/Person.java
+++ src/main/java/no/fint/model/felles/Person.java
@@ -1,14 +1,11 @@
 package no.fint.model.felles;
 
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
-import no.fint.model.felles.iso.Kjonn;
-import no.fint.model.relation.Identifiable;
+import no.fint.model.relation.FintModel;
 import no.fint.model.relation.Relation;
-import no.fint.model.relation.RelationType;
 
 import java.util.Date;
 import java.util.List;
@@ -16,22 +13,19 @@ import java.util.List;
 @Data
 @NoArgsConstructor
 @EqualsAndHashCode(callSuper = true)
-public class Person extends Aktor implements FintModel { // Relations (getRelations())
+public class Person extends Aktor implements FintModel {
 
-    public static enum Relasjonsnavn {
+    public enum Relasjonsnavn {
         PERSONALRESSURS,
         KJONN,
         STATSBORGERSKAP
     }
 
-    public static final String BASE = "https://api.felleskomponent.no/administrasjon/personal";
-
     private Identifikator fodselsnummer;
     private Personnavn navn;
     private Date fodselsdato;
     private Adresse bostedsadresse;
-    private List<Relation> relasjoner; // Relation.for(Person.Relasjonsnavn.PERSONALRESSURS).fromBase(BASE_URL).path("/personalressurs")
-                                        // Relation.for(Person.Relasjonsnavn.PERSONALRESSURS).link("http://localhost/personalressurs")
+    private List<Relation> relasjoner;
 
     public Person(Kontaktinformasjon kontaktinformasjon, Adresse postadresse, Identifikator fodselsnummer, Personnavn navn, Date fodselsdato, Adresse bostedsadresse) {
         super(kontaktinformasjon, postadresse);
@@ -41,6 +35,7 @@ public class Person extends Aktor implements FintModel { // Relations (getRelati
         this.bostedsadresse = bostedsadresse;
     }
 
+    @Override
     public List<Relation> getRelations() {
         return relasjoner;
     }
